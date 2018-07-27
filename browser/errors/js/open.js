e66362af162d86e0e379ea076e575dc0b4905706
AntonKC <ayk@ooott.ru>
2018-07-27 23:33:03 +0300

    antonkc: mail erroros

--- browser/errors/js/open.js
+++ browser/errors/js/open.js
@@ -1,11 +1,15 @@
 'use strict';
 function openMail() {
-  const values = ['Открыть', 'Закрыть'];
-  const btn = document.getElementById('open');
-  const mail = document.getElementById('mail');
-  mail.classList.toggle('is-active');
-  const state = mail.classList.contains('is-active') ? 1 : 0;
-  setTimeout(() => {
-    btn.value = values[state];
-  }, state * 2000);
+    console.log('1');
+    const values = ['Открыть', 'Закрыть'];
+    const btn = document.getElementById('open');
+    const mail = document.getElementById('mail');
+
+    mail.classList.toggle('is-active');
+    let state = mail.classList.contains('is-active') ? 1 : 0;
+
+    setTimeout(() => {
+        btn.value = values[state];
+    }, state * 2000);
 }
+
