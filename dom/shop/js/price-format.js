1bd0eaad8f926ce01b7241d3f5b5e32e307cd46b
AntonKC <ayk@ooott.ru>
2018-08-06 23:22:57 +0300

    antonkc: shop

--- dom/shop/js/price-format.js
+++ dom/shop/js/price-format.js
@@ -1,3 +1,18 @@
 function getPriceFormatted(value) {
-  return  value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+    return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
 }
+
+let cartCount = document.querySelector('#cart-count'),
+    cartTotalPrice = document.querySelector('#cart-total-price'),
+    result = 0;
+
+    console.log(cartCount);
+    console.log(cartTotalPrice);
+
+Array.from(document.querySelectorAll('.add')).forEach(function(item){
+    item.addEventListener('click', (e) => {
+        cartCount.innerHTML = getPriceFormatted(+cartCount.innerHTML + 1);
+        result += e.target.dataset.price * 1;
+        cartTotalPrice.innerHTML = getPriceFormatted(result);
+    });
+})
