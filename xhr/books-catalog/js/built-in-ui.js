af3e3c0c7a097f6da8821cf4dd6d84519ecc4419
AntonKC <ayk@ooott.ru>
2018-08-09 00:03:43 +0300

    antonkc: books-catalog

--- xhr/books-catalog/js/built-in-ui.js
+++ xhr/books-catalog/js/built-in-ui.js
@@ -1,5 +1,22 @@
 /* Данный JS код */
 
+let xhrBook = new XMLHttpRequest(),
+    content = document.querySelector('#content');
+
+xhrBook.addEventListener("load", onLoad);
+xhrBook.open("GET","https://neto-api.herokuapp.com/book/", true);
+xhrBook.send();
+
+function onLoad() {
+    let data = JSON.parse(xhrBook.responseText);
+    let str = '';
+    data.forEach(function (item) {
+        str += `<li data-title="${item.title}" data-author="${item.author.name}" data-info="${item.info}" data-price="${item.price}"><img src="${item.cover.small}"></li>`
+    })
+    content.innerHTML = str;
+};
+
+
 // Регулируем видимость карточки
 function toggleCardVisible () {
  document.getElementById('content').classList.toggle('hidden');
