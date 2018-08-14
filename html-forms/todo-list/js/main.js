cd4bb3bba4acb7c120c66cd0505ea1bf5a345f86
AntonKC <ayk@ooott.ru>
2018-08-14 23:02:08 +0300

    antonkc: html-forms

--- html-forms/todo-list/js/main.js
+++ html-forms/todo-list/js/main.js
@@ -8,14 +8,20 @@ const inputs = document.getElementsByTagName('INPUT');
 [].forEach.call(inputs, function(el){
     el.addEventListener('change', function (e) {
 
-        const taskFinished = document.getElementsByTagName('output');
+        const taskFinished = document.getElementById('out');
+        const allTaskFinished = document.getElementById('set');
 
         let count = 0;
         for (let i = 0; i < inputs.length; i++)
             if (inputs[i].type == 'checkbox' && inputs[i].checked)
                 count++;
-        taskFinished.value = count;
-        console.log(count);
+
+        if (count === inputs.length)
+            allTaskFinished.classList.toggle('complete');
+        else
+            allTaskFinished.classList.remove('complete');
+
+        taskFinished.value = `${count} из ${inputs.length}`;
     })
 });
 
