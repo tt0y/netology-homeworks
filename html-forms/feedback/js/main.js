b550ec515a08f33cc6bd59404f88f7a89bacd20e
AntonKC <ayk@ooott.ru>
2018-08-15 20:21:54 +0300

    antonkc: feedback

--- html-forms/feedback/js/main.js
+++ html-forms/feedback/js/main.js
@@ -1,8 +1,55 @@
 'use strict';
 
-const contentform = document.getElementsByClassName('contentform');
+const inputs = document.getElementsByTagName('input');
 const buttons = document.getElementsByClassName('button-contact');
 const output = document.getElementById('output');
-elements.addEventListener('change', function (el) {
-    el
-});
+const message = document.getElementsByName('message');
+const zipcode = document.getElementById('zipcode');
+
+
+document.addEventListener('DOMContentLoaded', initFields(inputs));
+zipcode.addEventListener('keyup', zipCodeFilter);
+
+function zipCodeFilter() {
+    console.log(zipcode.value);
+    let zip = parseInt(zipcode.value.replace(/\D+/g,""));
+    if (isNaN(zip))
+        zipcode.value = '';
+    else
+        zipcode.value = zip;
+}
+
+// Вешаем калбеки на все элементы формы
+function forEach(list, callback) {
+    Array.prototype.forEach.call(list, callback);
+}
+
+function initFields(elements) {
+    console.log('initFields');
+
+    // Повесим на все элементы лисенеры
+    forEach(elements, function (el) {
+        el.addEventListener('change', function (e) {
+            calcFillFields(inputs);
+        })
+    });
+}
+
+
+function calcFillFields(elements){
+     console.log('calcFillFields');
+
+    let count = 0;
+
+    for (let i = 0; i < elements.length; i++) {
+        if (elements[i].value !== '') {
+            count++;
+        }
+    }
+
+    if (count === elements.length && message.value !== ''){
+        for (let i = 0; i < buttons.length; i++)
+            if (buttons[i].type === 'submit')
+                buttons[i].disabled = false;
+    }
+}
