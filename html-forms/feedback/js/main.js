917112f2e909b81311d733809d6dbf3e4381da31
AntonKC <ayk@ooott.ru>
2018-08-19 23:53:56 +0300

    antonkc: feedback

--- html-forms/feedback/js/main.js
+++ html-forms/feedback/js/main.js
@@ -5,14 +5,14 @@ const buttons = document.getElementsByClassName('button-contact');
 const output = document.getElementById('output');
 const message = document.getElementsByName('message');
 const zipcode = document.getElementById('zipcode');
-
+const contentform = document.getElementById('form');
 
 document.addEventListener('DOMContentLoaded', initFields());
 zipcode.addEventListener('keyup', zipCodeFilter);
 
 // Фильтруем лишнее
 function zipCodeFilter() {
-    let zip = parseInt(zipcode.value.replace(/\D+/g,""));
+    let zip = parseInt(zipcode.value.replace(/\D+/g, ""));
     if (isNaN(zip))
         zipcode.value = '';
     else
@@ -25,8 +25,6 @@ function forEach(list, callback) {
 }
 
 function initFields() {
-    console.log('initFields');
-
     // Повесим на все элементы лисенеры
     forEach(inputs, function (inp) {
         inp.addEventListener('change', function (e) {
@@ -34,29 +32,47 @@ function initFields() {
         })
     });
 
+    forEach(message, function (mes) {
+        mes.addEventListener('change', function (e) {
+            sendValueByElementName(mes.name, mes.value);
+        })
+    });
+
     forEach(buttons, function (btn) {
         if (btn.type === 'submit')
-        btn.addEventListener('click', function (e) {
-            output.classList.remove('hidden')
-            output.classList.remove('hidden')
-        })
+            btn.addEventListener('click', function (e) {
+
+                if (output.classList.contains('hidden')) {
+                    output.classList.remove('hidden');
+                    contentform.classList.add('hidden');
+                } else {
+                    contentform.classList.remove('hidden');
+                    output.classList.add('hidden');
+                }
+            })
     });
 }
 
+function sendValueByElementName(name, value) {
+    let target = document.getElementById(name);
+    if (target !== null)
+        target.value = value;
+}
 
-function calcFillFields(elements){
-     console.log('calcFillFields');
-
+function calcFillFields(elements) {
     let count = 0;
 
     forEach(elements, function (el) {
         if (el.value !== '') {
             count++;
+            sendValueByElementName(el.name, el.value);
         }
     });
 
     forEach(buttons, function (btn) {
-        if (btn.type === 'submit' && count === elements.length)
+        if (btn.type === 'submit' && count === elements.length) {
             btn.disabled = false;
+        }
     });
+
 }
