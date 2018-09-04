c5c64fe366b6425f16dac756a254f14d2b8b1fef
AntonKC <ayk@ooott.ru>
2018-09-04 22:26:13 +0300

    antonkc: event-bubbling-capturing

--- event-bubbling-capturing/menu/js/menu.js
+++ event-bubbling-capturing/menu/js/menu.js
@@ -1,34 +1,38 @@
 'use strict';
 
 function toggleMenu(event) {
-  if (this.classList.contains('show')) {
-    this.classList.remove('show');
-    this.classList.add('hide');
-  } else {
-    this.classList.add('show');
-    this.classList.remove('hide');
-  }
+    const current = event.currentTarget;
+
+    if (current.classList.contains('show')) {
+        current.classList.remove('show');
+        current.classList.add('hide');
+    } else {
+        current.classList.add('show');
+        current.classList.remove('hide');
+    }
 }
 
 function openLink(event) {
-  console.log(this.textContent);
+    event.preventDefault();
+    console.log(this.textContent);
+    event.stopPropagation();
 }
 
 function init(node) {
-  node.addEventListener('click', toggleMenu);
+    node.addEventListener('click', toggleMenu);
 }
 
 function initLink(node) {
-  if (node.dataset.toggle) {
-    return;
-  }
-  node.addEventListener('click', openLink);
+    if (node.dataset.toggle) {
+        return;
+    }
+    node.addEventListener('click', openLink, true);
 }
 
 Array
-  .from(document.querySelectorAll('.dropdown'))
-  .forEach(init);
+    .from(document.querySelectorAll('.dropdown'))
+    .forEach(init);
 
 Array
-  .from(document.querySelectorAll('a'))
-  .forEach(initLink);
+    .from(document.querySelectorAll('a'))
+    .forEach(initLink);
--- event-bubbling-capturing/shop/js/cart.js
+++ event-bubbling-capturing/shop/js/cart.js
@@ -1,1 +1,18 @@
 'use strict';
+
+list.addEventListener('click', buttonAddToCart);
+
+function buttonAddToCart(event) {
+    event.preventDefault();
+
+    if (!event.target.classList.contains('add-to-cart')) {
+        return;
+    }
+
+    const itemData = {
+        title: event.target.dataset.title,
+        price: event.target.dataset.price
+    };
+
+    addToCart(itemData);
+}
