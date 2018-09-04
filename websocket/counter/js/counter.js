d837946aa8ab5a6ec132fc3efa568019c7357c53
AntonKC <ayk@ooott.ru>
2018-09-04 22:32:32 +0300

    antonkc: websocket

--- websocket/counter/js/counter.js
+++ websocket/counter/js/counter.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const ws = new WebSocket('wss://neto-api.herokuapp.com/counter');
+const counterOutput = document.querySelector('.counter');
+const errorsOutput = document.querySelector('.errors');
+
+window.addEventListener('beforeunload', () => {
+    ws.close(1000, 'Работа закончена.');
+});
+
+ws.addEventListener('message', (event) => {
+    const data = JSON.parse(event.data);
+    counterOutput.textContent = data.connections;
+    errorsOutput.textContent = data.errors;
+});
